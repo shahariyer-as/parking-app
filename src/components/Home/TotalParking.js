660dcf2ed7242065a62cace0d3df3f9037fc748a
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2022-10-26 15:51:49 +0600

    some fixed

--- src/components/Home/TotalParking.js
+++ src/components/Home/TotalParking.js
@@ -3,10 +3,9 @@ import React, { useEffect, useState } from "react";
 const TotalParking = () => {
   const [services, setServices] = useState([]);
   const [checkOutDate, setCheckOutDate] = useState({});
-  const [available,setAvailable]=useState([])
+  const [available, setAvailable] = useState([]);
   // console.log("checkOutDate", checkOutDate)
-  const [id,setId]=useState('')
-  
+  const [id, setId] = useState("");
 
   useEffect(() => {
     fetch("https://parking-app-server.vercel.app/service")
@@ -14,47 +13,49 @@ const TotalParking = () => {
       .then((data) => setServices(data));
   }, [services]);
 
-// available parking 
+  // available parking
 
-    useEffect(()=>{
-        fetch('https://parking-app-server.vercel.app/available').then(res=>res.json()).then(data=> setAvailable(data))
-    },[])
+  useEffect(() => {
+    fetch("https://parking-app-server.vercel.app/available")
+      .then((res) => res.json())
+      .then((data) => setAvailable(data));
+  }, []);
 
-// put check out time 
-useEffect(()=>{
-  // console.log(checkOutDate,id)
-  if(checkOutDate){
-    fetch(`https://parking-app-server.vercel.app/${id}`,{
-    method: "PUT",
-    headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(checkOutDate),
-}).then((data) => {
-    if(data.status===200){
-      window.alert("checkOut successfully")
-      setServices()
+  // put check out time
+  useEffect(() => {
+    // console.log(checkOutDate,id)
+    if (checkOutDate) {
+      fetch(`https://parking-app-server.vercel.app/parking/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkOutDate),
+      }).then((data) => {
+        if (data.status === 200) {
+          window.alert("checkOut successfully");
+          setServices();
+        }
+      });
     }
-   
-  });
-  }
- 
-},[checkOutDate,id,setServices])
+  }, [checkOutDate, id, setServices]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const date = e.target.date.value;
+    setCheckOutDate({ date });
+  };
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const date =e.target.date.value
-  setCheckOutDate({date});
-};
+  // const getSingeId=id=>{
+  //   setId(id)
+  //  }
 
-// const getSingeId=id=>{
-//   setId(id)
-//  }
- 
   return (
     <div>
-    <h2 className='text-red-500 text-3xl text-center'> Available In Parking Vehicles:{available.length - services.length}  </h2>
+      <h2 className="text-red-500 text-3xl text-center">
+        {" "}
+        Available In Parking Vehicles:{available.length - services.length}{" "}
+      </h2>
       <div className="pt-5 overflow-x-auto">
         <table className="table w-full">
           <thead>
@@ -88,7 +89,7 @@ const handleSubmit = (e) => {
                       <input
                         className="btn  bg-primary max-w-xs text-white"
                         type="submit"
-                        value="Submit"
+                        value="CheckOut"
                         onClick={() => setId(s._id)}
                       />
                     </form>
